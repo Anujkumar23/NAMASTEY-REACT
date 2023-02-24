1f23115b0754b5ec737bb906934cb225174eeaf1
anuj <anujthakur315@gmail.com>
2023-02-24 12:17:53 +0530

    Footer Component

---
+++ Chapter 5 - Let's get Hooked/Component/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="footer">
+          
+    created by💘
+    <a className="link" href="https://www.linkedin.com/in/anuj-kumar-76509a20b/">Anuj Kumar</a> 
+      <span className="copy"> © </span>
+     2023 <strong className="logo-name"> Hot<span>Spot</span></strong>
+</footer>
+  )
+}
+
+export default Footer
