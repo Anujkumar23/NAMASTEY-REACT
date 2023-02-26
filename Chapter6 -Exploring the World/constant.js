7c30318a184711e91d191534aff66805290e93ae
anuj <anujthakur315@gmail.com>
2023-02-26 22:02:50 +0530

    API

---
+++ Chapter6 -Exploring the World/constant.js
@@ -0,0 +1,4 @@
+export const SWIGGY_API_URL =
+  "https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.2587531&lng=75.78041&page_type=DESKTOP_WEB_LISTING";
+
+  export const  IMG_CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
