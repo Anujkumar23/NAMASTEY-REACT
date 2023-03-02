9e7b0f409a37b3473a16ec688ffce3e728dc8145
anuj <anujthakur315@gmail.com>
2023-03-03 00:10:05 +0530

    add contact page

---
+++ Chapter 7 - Finding the Path/Pages/Contact.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import contactImg from "../Images/Contact-Us.png"
+
+function Contact() {
+  const [message,setMessage]=useState(false)
+
+
+  const handleSubmit=(e)=>{
+      e.preventDefault();
+      setMessage(true)
+      
+
+
+
+  }
+
+
+
+  return (
+    <div className='contact-container'>
+      <div className='contact-left'>
+        <img src={contactImg} alt="contact-img" />
+      </div>
+      <div className='contact-right'>
+        <h1>Contact</h1>
+        <form action="" onSubmit={handleSubmit}>
+          <input type="text" placeholder='Full name' name='name' id="name" required />
+          <input type="email" placeholder='email' name='email' id='email' required />
+          <textarea name="textarea" id="textarea" cols="5" rows="5" required/>
+          <button type='submit'>Submit</button>
+        </form>
+       { message && <h1>Thanks for contacting us</h1>}
+      </div>
+
+    </div>
+  )
+}
+
+export default Contact
